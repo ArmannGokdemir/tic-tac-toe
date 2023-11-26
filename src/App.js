1258ab54d6a742ecd55f465b8eb2ecba5180382f
ArmannGokdemir <armangokde@gmail.com>
2023-11-26 22:32:41 +0300

    declaring winner

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import { useState } from "react";
 
+function Square({ value, onSquareClick }) {
+  // const [value, setValue] = useState(null);
+
+  // function handleClick() {
+  //   setValue("X");
+  // }
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
+    if (nextSquares[i] || calculateWinner(squares)) return;
     nextSquares[i] = nextSquares[i] || xIsNext ? "X" : "O";
     setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
+
+  const winner = calculateWinner(squares);
+  let status = winner
+    ? `Winner: ${winner}`
+    : `Next Player :  ${xIsNext ? "X" : "O"}`;
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -31,15 +52,22 @@ export default function Board() {
   );
 }
 
-function Square({ value, onSquareClick }) {
-  // const [value, setValue] = useState(null);
-
-  // function handleClick() {
-  //   setValue("X");
-  // }
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
